9335ca557d8f0405fab6e3a9fea16fd3e7587cbd
Zac Warner <45380112+geminerald@users.noreply.github.com>
2021-09-23 00:00:18 +0100

    fixed bug where text was not showing in email by adding correct indexing

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter.constants import END
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 import win32com.client as client
 
@@ -9,11 +10,13 @@ def send_mail():
     message.To = email_entry.get()
     message.CC = manager_entry.get()
     message.Subject = subject_entry.get()
-    message.Body = text_entry.get()
+    message.Body = text_entry.get(0.0, END)
 
 
 window = tk.Tk()
 window.title("Onboarder")
+window.rowconfigure(0, minsize=800, weight=1)
+window.columnconfigure(0, minsize=800, weight=1)
 
 container_frame = tk.Frame()
 container_frame.pack()
@@ -39,11 +42,13 @@ subject_entry = tk.Entry(master=row_two)
 text_label = tk.Label(master=row_two, text="Enter Message Body")
 text_entry = tk.Text(master=row_two)
 
-text_entry.pack()
-text_label.pack()
 
-subject_entry.pack()
 subject_label.pack()
+subject_entry.pack()
+
+text_label.pack()
+text_entry.pack()
+
 
 manager_label.pack()
 manager_entry.pack()
